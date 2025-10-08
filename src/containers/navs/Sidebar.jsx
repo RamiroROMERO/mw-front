0444eca1b08d7ba4c2d309f2f22e21c912d9b355
MichellRM <michell.recarte@gmail.com>
2025-10-08 14:21:19 -0600

    fix sidebar

--- src/containers/navs/Sidebar.jsx
+++ src/containers/navs/Sidebar.jsx
@@ -15,7 +15,7 @@ const fnValidMenuItems = (menuItems) => {
   const companyData = JSON.parse(localStorage.getItem('mw_current_company'));
   if (companyData) {
     const { enableBankMenu, enableContabMenu, enableFixedAssetsMenu, enableHospitalMenu, enableInventoryMenu, enableInvoiceMenu,
-      enableLabMenu, enableLoansMenu, enableRRHHMenu, enableTaxMenu } = companyData;
+      enableLabMenu, enableLoansMenu, enableRRHHMenu, enableTaxMenu, enableHotelMenu } = companyData;
     if (!enableBankMenu) {
       menuItems = menuItems.filter(menu => menu.id !== 'banks');
     }
@@ -54,6 +54,10 @@ const fnValidMenuItems = (menuItems) => {
 
     if (!enableLoansMenu) {
       menuItems = menuItems.filter(menu => menu.id !== 'loans');
+    };
+
+    if (!enableHotelMenu) {
+      menuItems = menuItems.filter(menu => menu.id !== 'hotelManagement');
     };
 
     return menuItems;
