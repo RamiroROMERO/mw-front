804f151b64c1fa9b1d7d7414873b1ea7b8650be3
Ramiro Romero <rromero.dev@hotmail.com>
2026-01-12 18:03:23 -0600

    url for hotel quotes.

--- src/helpers/APIUrl.js
+++ src/helpers/APIUrl.js
@@ -1,4 +1,6 @@
 export const API_URLS = {
   "FAC_PROC_QUOTES": "billing/process/quotes/",
-  "FAC_PROC_QUOTES_DETAIL": "billing/process/quoteDetails/"
+  "FAC_PROC_QUOTES_DETAIL": "billing/process/quoteDetails/",
+  "HOTEL_PROC_QUOTES": 'hotel/process/quotes',
+  "HOTEL_PROC_QUOTE_DETAIL": 'hotel/process/quoteDetails',
 };
